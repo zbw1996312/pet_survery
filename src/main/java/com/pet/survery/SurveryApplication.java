8ca57296572f37655d3e65d0e80f5164e1c42c94
张博炜 <zhangbowei@zhangboweideMacBook-Pro.local>
2020-09-09 17:51:19 +0800

    Initial commit

---
+++ src/main/java/com/pet/survery/SurveryApplication.java
@@ -0,0 +1,13 @@
+package com.pet.survery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pet/survery/SurveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.survery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
